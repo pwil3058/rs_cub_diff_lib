00e0df98ac923a6dd5725d4e73b7ea86cc3e9b4f
Peter Williams <pwil3058@gmail.com>
2020-04-22 09:31:22 +1000

    cub_diff:change: add test for clean application of changes
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- cub_diff/src/change.rs
+++ cub_diff/src/change.rs
@@ -6,7 +6,7 @@ use crate::lines::{
     first_inequality_fm_head, first_inequality_fm_tail, CompleteLinesExt, LinesIfce,
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Snippet<'a> {
     start_index: usize,
     lines: Vec<&'a str>,
@@ -22,15 +22,23 @@ impl<'a> Snippet<'a> {
         &self,
         lines: &[&str],
         start_index: usize,
-        context: Context,
-    ) -> Option<(usize, usize, usize)> {
+        context: &Context,
+    ) -> Option<(Self, Context)> {
         for head_redn in 0..context.head_len {
             for tail_redn in 0..context.tail_len {
                 let to_index = self.lines.len() - tail_redn;
                 if let Some(found_start_index) =
                     lines.find_first_sub_lines(&self.lines[head_redn..to_index], start_index)
                 {
-                    return Some((found_start_index, head_redn, tail_redn));
+                    let snippet = Self {
+                        start_index: found_start_index,
+                        lines: self.lines[head_redn..to_index].to_vec(),
+                    };
+                    let context_redn = Context {
+                        head_len: head_redn,
+                        tail_len: tail_redn,
+                    };
+                    return Some((snippet, context_redn));
                 }
             }
         }
@@ -38,7 +46,7 @@ impl<'a> Snippet<'a> {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone, Copy)]
 pub struct Context {
     pub head_len: usize,
     pub tail_len: usize,
@@ -69,7 +77,7 @@ impl From<(&Vec<&str>, &Vec<&str>)> for Context {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Change<'a, 'b> {
     ante_snippet: Snippet<'a>,
     post_snippet: Snippet<'b>,
@@ -112,11 +120,26 @@ pub fn apply_changes(changes: &[Change], text: &str) -> Changed {
             for line in &post.lines {
                 changed.string += line;
             }
-            lines_index =
-                (((ante.start_index + ante.lines.len()) as isize) + current_offset) as usize;
+            lines_index = index + ante.lines.len();
             changed.successes += 1;
             continue;
         };
+        // if let Some((reduced_snippet, context_redn)) =
+        //     ante.match_lines_fuzzy(&lines, lines_index, &change.context)
+        // {
+        //     for line in &lines[lines_index..reduced_snippet.start_index] {
+        //         changed.string += line;
+        //     }
+        //     let end = post.lines.len() - context_redn.tail_len;
+        //     for line in &post.lines[context_redn.head_len..end] {
+        //         changed.string += line;
+        //     }
+        //     Broken
+        //     lines_index = reduced_snippet.start_index + end;
+        //     current_offset = reduced_snippet.start_index as isize - ante.start_index as isize;
+        //     changed.merges += 1;
+        //     continue;
+        // }
         // if let Some((index, head_context_redn, tail_context_redn)) = ante.match_lines_fuzzy(
         //     &lines,
         //     lines_index,
@@ -156,6 +179,9 @@ pub fn apply_changes(changes: &[Change], text: &str) -> Changed {
         //     continue;
         // }
     }
+    for line in lines[lines_index..].iter() {
+        changed.string += line;
+    }
     changed
 }
 
@@ -168,5 +194,125 @@ pub struct AppliedPosnData {
 impl fmt::Display for AppliedPosnData {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "line {} ({} lines)", self.start_posn, self.length)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::change::{apply_changes, Change, Snippet};
+    use crate::lines::CompleteLinesExt;
+
+    #[test]
+    fn test_change_application() {
+        let ante_text =
+            "zero\none\ntwo\nthree\nfour\nfive\nsix\nseven\neight\nnine\nten\neleven\ntwelve"
+                .to_string();
+        let ante_lines: Vec<&str> = ante_text.complete_lines().collect();
+        let post_text =
+            "zero\none\ntwo\nthree mod\nfour\nfive\nextra\nsix\nseven\neight mod\nnine mod\nten\neleven\ntwelve\nextra\n".to_string();
+        let post_lines: Vec<&str> = post_text.complete_lines().collect();
+        let ante_snippets = vec![
+            Snippet {
+                start_index: 2,
+                lines: ante_lines[2..5].to_vec(),
+            },
+            Snippet {
+                start_index: 5,
+                lines: ante_lines[5..7].to_vec(),
+            },
+            Snippet {
+                start_index: 7,
+                lines: ante_lines[7..11].to_vec(),
+            },
+            Snippet {
+                start_index: 12,
+                lines: ante_lines[12..].to_vec(),
+            },
+        ];
+        println!("ante snippets: {:?}", ante_snippets);
+        let post_snippets = vec![
+            Snippet {
+                start_index: 2,
+                lines: post_lines[2..5].to_vec(),
+            },
+            Snippet {
+                start_index: 5,
+                lines: post_lines[5..8].to_vec(),
+            },
+            Snippet {
+                start_index: 8,
+                lines: post_lines[8..12].to_vec(),
+            },
+            Snippet {
+                start_index: 13,
+                lines: post_lines[13..].to_vec(),
+            },
+        ];
+        println!("post snippets: {:?}", post_snippets);
+        let mut changes: Vec<Change> = vec![];
+        for (a, p) in ante_snippets.iter().zip(post_snippets.iter()) {
+            let change = Change::new(a.clone(), p.clone());
+            changes.push(change);
+        }
+        let changes = apply_changes(&changes, &ante_text);
+        assert_eq!(changes.string, post_text);
+        //assert!(false);
+    }
+
+    #[test]
+    fn test_change_application_with_tail() {
+        let ante_text =
+            "zero\none\ntwo\nthree\nfour\nfive\nsix\nseven\neight\nnine\nten\neleven\ntwelve\nthirteen\nfourteen\n"
+                .to_string();
+        let ante_lines: Vec<&str> = ante_text.complete_lines().collect();
+        let post_text =
+            "zero\none\ntwo\nthree mod\nfour\nfive\nextra\nsix\nseven\neight mod\nnine mod\nten\neleven\ntwelve\nextra\nthirteen\nfourteen\n".to_string();
+        let post_lines: Vec<&str> = post_text.complete_lines().collect();
+        let ante_snippets = vec![
+            Snippet {
+                start_index: 2,
+                lines: ante_lines[2..5].to_vec(),
+            },
+            Snippet {
+                start_index: 5,
+                lines: ante_lines[5..7].to_vec(),
+            },
+            Snippet {
+                start_index: 7,
+                lines: ante_lines[7..11].to_vec(),
+            },
+            Snippet {
+                start_index: 12,
+                lines: ante_lines[12..14].to_vec(),
+            },
+        ];
+        println!("ante snippets: {:?}", ante_snippets);
+        let post_snippets = vec![
+            Snippet {
+                start_index: 2,
+                lines: post_lines[2..5].to_vec(),
+            },
+            Snippet {
+                start_index: 5,
+                lines: post_lines[5..8].to_vec(),
+            },
+            Snippet {
+                start_index: 8,
+                lines: post_lines[8..12].to_vec(),
+            },
+            Snippet {
+                start_index: 13,
+                lines: post_lines[13..16].to_vec(),
+            },
+        ];
+        println!("post snippets: {:?}", post_snippets);
+        let mut changes: Vec<Change> = vec![];
+        for (a, p) in ante_snippets.iter().zip(post_snippets.iter()) {
+            let change = Change::new(a.clone(), p.clone());
+            changes.push(change);
+        }
+        let changes = apply_changes(&changes, &ante_text);
+        assert_eq!(changes.string, post_text);
+        //assert!(false);
     }
 }
